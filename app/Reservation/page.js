b92d9d3175cb4fb4606187cc281cac7f1fc01083
Ajiboye Ismail <ishmaeldotun@gmail.com>
2026-04-24 14:03:01 +0100

    fix

--- app/Reservation/page.js
+++ app/Reservation/page.js
@@ -2,13 +2,11 @@
 import DateSelector from "../Dateselector/page";
 import ReservationForm from "../Reservationform/page";
 
-const Reservation = ({ cab }) => {
+export default function Reservation({ cab }) {
   return (
     <div className="md:grid grid-cols-2 mt-10">
       <DateSelector />
       <ReservationForm cab={cab} />
     </div>
   );
-};
-
-export default Reservation;
+}
